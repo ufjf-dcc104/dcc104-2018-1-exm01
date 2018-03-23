f86bcd9c2afb96914ee1704d097f409252a29eb3
ICE <ice@lab05m51.nrc.ice.ufjf.br>
2018-03-23 20:18:19 -0300

    Adiciona vários sprites

--- sprite.js
+++ sprite.js
@@ -5,17 +5,17 @@ function Sprite() {
   this.h = 20;
   this.vx = 0;
   this.vy = 0;
+}
 
-  this.desenhar = function (ctx) {
-    ctx.fillStyle = "grey";
-    ctx.strokeStyle = "white";
-    ctx.lineWidth = 3;
-    ctx.fillRect(this.x, this.y, this.w, this.h);
-    ctx.strokeRect(this.x, this.y, this.w, this.h);
-  }
+Sprite.prototype.desenhar = function (ctx) {
+  ctx.fillStyle = "grey";
+  ctx.strokeStyle = "white";
+  ctx.lineWidth = 3;
+  ctx.fillRect(this.x, this.y, this.w, this.h);
+  ctx.strokeRect(this.x, this.y, this.w, this.h);
+}
 
-  this.mover = function (dt) {
-      this.x = this.x + this.vx*dt;
-      this.y = this.y + this.vy*dt;
-  }
+Sprite.prototype.mover = function (dt) {
+    this.x = this.x + this.vx*dt;
+    this.y = this.y + this.vy*dt;
 }
